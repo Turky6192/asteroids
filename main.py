0556bf09edb188d715986b4dfc7649f985fd49a6
Turky6192 <widowmaker61@hotmail.com>
2025-04-03 12:07:48 -0400

    added Player

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
@@ -9,6 +11,7 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     dt = 0
     clock = pygame.time.Clock()
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -16,6 +19,8 @@ def main():
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        player.draw(screen)
+
 
         
 
